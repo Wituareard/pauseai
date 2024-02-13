ff84f11376ecbef4da180b9041aeb150f5dde9a1
Wituareard <87208350+Wituareard@users.noreply.github.com>
2024-02-13 09:22:41 +0100

    Optimize lucide-svelte imports

--- src/lib/components/SearchButton.svelte
+++ src/lib/components/SearchButton.svelte
@@ -1,6 +1,6 @@
 <script>
     import NavLink from "$lib/components/Navlink.svelte";
-    import { Search } from 'lucide-svelte'
+    import Search from 'lucide-svelte/icons/search'
 </script>
 
 <NavLink href="/search"><Search size="0.8em" /></NavLink>
